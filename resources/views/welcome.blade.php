1fbe31225d03111ce169a3b7a9c61ce06bc58798
vagrant <vagrant@homestead>
2015-04-04 15:21:51 +0000

    updated welcome blade so i know which project im in

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
+				<div class="title">Toronto Music Club</div>
 				<div class="quote">{{ Inspiring::quote() }}</div>
 			</div>
 		</div>
