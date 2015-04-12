55d99291a42ede062e077be078a1dbeae3022bc3
NightMayor <devinnewbery@gmail.com>
2015-04-12 23:34:17 +0000

    added a few columns to subscriptions migration

--- database/migrations/2015_04_11_212929_create_subscriptions_table.php
+++ database/migrations/2015_04_11_212929_create_subscriptions_table.php
@@ -16,6 +16,10 @@ class CreateSubscriptionsTable extends Migration {
 		{
 			$table->increments('id');
 			$table->string('description');
+			$table->integer('primary_instruments')->unsigned();
+			$table->integer('secondary_instruments')->unsigned();
+			$table->integer('seeking_posts')->unsigned();
+			$table->integer('available_posts')->unsigned();
 			$table->timestamps();
 			$table->softDeletes();
 		});
