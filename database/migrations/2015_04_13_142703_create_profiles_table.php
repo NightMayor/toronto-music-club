f9c758d816d04ce6549eeb40db73196e4a86c316
NightMayor <devinnewbery@gmail.com>
2015-04-13 14:46:16 +0000

    Migration for profiles table

---
+++ database/migrations/2015_04_13_142703_create_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('profiles', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->string('first_name');
+			$table->string('last_name');
+			$table->string('avatar')->nullable();
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists();
+	}
+
+}
