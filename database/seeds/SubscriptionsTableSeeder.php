d5ec7aef79d82666022a0fc7a6cd3bc2216e30f6
NightMayor <devinnewbery@gmail.com>
2015-04-13 18:29:29 +0000

    Seed and Model for Subscription table created

---
+++ database/seeds/SubscriptionsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Subscription;
+
+class SubscriptionsTableSeeder extends Seeder {
+
+    public function run()
+    {
+    	DB::table('subscriptions')->delete();
+
+    	$subscriptions = [
+    		[
+                'description'           => 'Basic',
+                'primary_instruments'   => 2,
+                'secondary_instruments' => 5,
+                'seeking_posts'         => 4,
+                'available_posts'       => 3,
+    		],
+    		[
+                'description'           => 'Professional',
+                'primary_instruments'   => 5,
+                'secondary_instruments' => 10,
+                'seeking_posts'         => 10,
+                'available_posts'       => 10,
+    		]
+    	];
+
+        foreach ($subscriptions as $subscription) {
+            Subscription::create($subscription);
+        }
+    }
+}
