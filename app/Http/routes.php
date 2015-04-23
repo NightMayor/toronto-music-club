e28f4a2b58d06d6c8d425f40e3c1329d4a2377a8
NightMayor <devinnewbery@gmail.com>
2015-04-23 16:54:34 +0000

    Controllers created

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+Route::resource('instruments', 'InstrumentsController');
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
