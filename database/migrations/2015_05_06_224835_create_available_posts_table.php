7fd20e498a45bedaf1ad6cf624bc5fcb6c9ead7e
NightMayor <devinnewbery@gmail.com>
2015-05-10 15:48:17 +0000

    Migration for available posts table

--- database/migrations/2015_05_06_224835_create_available_posts_table.php
+++ database/migrations/2015_05_06_224835_create_available_posts_table.php
@@ -15,7 +15,14 @@ class CreateAvailablePostsTable extends Migration {
 		Schema::create('available_posts', function(Blueprint $table)
 		{
 			$table->increments('id');
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->integer('instrument_id')->unsigned();
+			$table->foreign('instrument_id')->references('id')->on('instruments');
+			$table->string('title', 50);
+			$table->string('body', 500);
 			$table->timestamps();
+			$table->softDeletes();
 		});
 	}
 
