c4651b107dc288f1aeb3c66713212d781c9c4d6c
NightMayor <devinnewbery@gmail.com>
2015-05-26 20:29:28 +0000

    Avatar is accessable

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,8 @@
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
 				<div class="panel-heading">Home</div>
-
 				<div class="panel-body">
+					<img src = "/images/avatars/{{ $stuff['id'] }}/avatar.jpg">
 					{{ $stuff['name'] }}, you are logged in!
 				</div>
 			</div>
