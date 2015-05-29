d88f6362f8841b138453cdfc390b2c1ef71d7285
NightMayor <devinnewbery@gmail.com>
2015-05-29 16:08:25 +0000

    Added api prefix to routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,18 +15,21 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
-Route::group([
-	'middleware' => 'auth',
-	], function() {
-		resource('instruments', 'InstrumentsController');
-		resource('profile', 'ProfileController');
-		resource('genders', 'GendersController');
-		resource('subscriptions', 'SubscriptionsController');
-		resource('available-posts', 'AvailablePostsController');
-		resource('seeking-posts', 'SeekingPostsController');
-		resource('threads', 'ThreadsController');
-	}
-);
+Route::group(array('prefix' => 'api'), function() {
+
+	Route::group([
+		'middleware' => 'auth',
+		], function() {
+			resource('instruments', 'InstrumentsController');
+			resource('profile', 'ProfileController');
+			resource('genders', 'GendersController');
+			resource('subscriptions', 'SubscriptionsController');
+			resource('available-posts', 'AvailablePostsController');
+			resource('seeking-posts', 'SeekingPostsController');
+			resource('threads', 'ThreadsController');
+		}
+	);
+});
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
